1d36e471e4e0bbb1ab3c6962a11385b96fe3cd47
Ruben Poppe <ruben.poppe@hotmail.com>
2021-05-20 11:34:54 +0200

    format

--- server.js
+++ server.js
@@ -5,32 +5,36 @@ const { nanoid } = require('nanoid');
 const { unlink } = require('fs/promises');
 
 const app = express();
-app.use(express.static('public', {
-    setHeaders: (res, path, status) => {
-        res.set('Content-Disposition', 'attachment');
-    }
-}));
+app.use(
+	express.static('public', {
+		setHeaders: (res, path, status) => {
+			res.set('Content-Disposition', 'attachment');
+		},
+	})
+);
 app.use(cors());
 app.disable('x-powered-by');
 
 const upload = multer({
-    storage: multer.diskStorage({
-        destination: 'public/img/',
-        filename: (req, file, cb) => cb(null, `${nanoid(16)}.jpg`)
-    })
+	storage: multer.diskStorage({
+		destination: 'public/img/',
+		filename: (req, file, cb) => cb(null, `${nanoid(16)}.jpg`),
+	}),
 });
 
 app.post('/upload', upload.array('images'), (req, res) => {
-    res.status(200).json(req.files.map(x => ({ [x.originalname]: `/img/${x.filename}` })));
+	res
+		.status(200)
+		.json(req.files.map((x) => ({ [x.originalname]: `/img/${x.filename}` })));
 });
 
 app.delete('/img/:path', async (req, res) => {
-    try {
-        await unlink(`public/img/${req.params.path}`);
-        res.status(204).json(`${req.params.path} deleted`);
-    } catch (err) {
-        res.status(500).json('something went wrong');
-    }    
+	try {
+		await unlink(`public/img/${req.params.path}`);
+		res.status(204).json(`${req.params.path} deleted`);
+	} catch (err) {
+		res.status(500).json('something went wrong');
+	}
 });
 
 app.listen(process.env.PORT || 3001);
